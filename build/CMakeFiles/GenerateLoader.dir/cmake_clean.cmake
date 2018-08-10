584d91cf4567bd5dfce95a6d9bac77e3f0188bfe
Josef Schmeißer <josef.schmeisser@tum.de>
2018-08-10 20:05:18 +0200

    cleanup

--- build/CMakeFiles/GenerateLoader.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/GenerateLoader"
-  "loader_generated.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/GenerateLoader.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/standalone_tests/CMakeFiles/phi_test.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for phi_test.
-# This may be replaced when dependencies are built.
