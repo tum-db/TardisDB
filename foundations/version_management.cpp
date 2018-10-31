3022e18531f6f5118c745bd6efae9828c13e3590
Josef Schmeißer <josef.schmeisser@tum.de>
2018-10-31 21:33:13 +0100

    interface specification

--- foundations/version_management.cpp
+++ foundations/version_management.cpp
@@ -1,13 +1,43 @@
 #include "foundations/version_management.hpp"
 
-tid_t insert(std::vector<SqlValue> tuple, branch_id_t branch, Table & table) {
+#include "utils/tagged_ptr.hpp"
+
+tid_t insert(std::vector<Cpp::Sql::SqlValue> tuple, branch_id_t branch, Table & table) {
     // TODO
 }
 
-void update(tid_t tid, std::vector<SqlValue> tuple, branch_id_t branch, Table & table) {
+void update(tid_t tid, std::vector<Cpp::Sql::SqlValue> tuple, branch_id_t branch, Table & table) {
     // TODO
 }
 
-void delete_tuple(tid_t tid, std::vector<SqlValue> tuple, branch_id_t branch, Table & table) {
+void delete_tuple(tid_t tid, std::vector<Cpp::Sql::SqlValue> tuple, branch_id_t branch, Table & table) {
     // TODO
+}
+
+Cpp::Sql::SqlTuple get_latest_tuple(tid_t tid, branch_id_t branch, Tale & table) {
+    if (branch == master_branch_id) {
+        throw 0; // use a regular scan
+    }
+
+    void * next = table.mv_begin[tid];
+
+    while (next != nullptr) {
+        tagged_ptr chain(next);
+
+        if (likely(chain.is_tagged()) {
+            const auto storage = static_cast<const VersionedTupleStorage *>(chain.untag())
+            if (storage.branchIndicators.test(branch)) {
+                return Cpp::Sql::SqlTuple::load(storage.tuple);
+            }
+            next = storage->...;
+        } else {
+            const auto & branchBitmap = table.getBranchBitmap();
+            if (branchBitmap.isSet(tid, branch)) {
+                // TODO
+            }
+            next = table.mv_next[tid];
+        }
+
+
+    }
 }
