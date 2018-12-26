73b038d30a8b3b59e8c03f95fb8fac2acd47194a
Josef Schmeißer <josef.schmeisser@tum.de>
2018-12-26 21:54:10 +0100

    updated benchmark

--- benchmark/random.cpp
+++ benchmark/random.cpp
@@ -8,10 +8,14 @@
 #include "native/sql/SqlValues.hpp"
 #include "native/sql/SqlTuple.hpp"
 
+static constexpr size_t repetitions = 16;
+
 static constexpr auto master_factor = 0.7;
-static constexpr auto update_factor = 0.2;
+//static constexpr auto update_factor = 0.2;
+static constexpr auto update_factor = 5.0;
 static constexpr int seed = 42;
-static constexpr size_t new_tuples_per_episode = 1<<18;
+//static constexpr size_t new_tuples_per_episode = 1<<18;
+static constexpr size_t new_tuples_per_episode = 1<<19;
 
 static std::mt19937 rd_engine(seed);
 
@@ -149,32 +153,19 @@ inline void print_result(const std::tuple<TmplScanItem<Integer>, TmplScanItem<In
     );
 }
 
-void measure_master_scan_yielding_latest(Database & db, Table & table) {
-    QueryContext ctx(db);
-    ctx.executionContext.branchId = master_branch_id;
-    db.constructBranchLineage(master_branch_id, ctx.executionContext);
-
-    const auto & column0 = table.getColumn(0);
-    const auto & column1 = table.getColumn(1);
-    const auto & column2 = table.getColumn(2);
-
-    auto scan_items = std::make_tuple<
-        TmplScanItem<Integer>, TmplScanItem<Integer>, TmplScanItem<Integer>>(
-        {column0, 0},
-        {column1, 4},
-        {column2, 8});
-
-    using namespace std::chrono;
-    const auto query_start = high_resolution_clock::now();
-    scan_relation_yielding_latest(ctx, table, print_result, scan_items);
-    const auto query_duration = high_resolution_clock::now() - query_start;
-    printf("Execution time: %lums\n", duration_cast<milliseconds>(query_duration).count());
+static size_t tuple_cnt = 0;
+static int sum = 0;
+inline void sum_consumer(const std::tuple<TmplScanItem<Integer>, TmplScanItem<Integer>, TmplScanItem<Integer>> & scan_items) {
+    sum += std::get<0>(scan_items).reg.sql_value.value;
+    sum += std::get<1>(scan_items).reg.sql_value.value;
+    sum += std::get<2>(scan_items).reg.sql_value.value;
+    tuple_cnt += 1;
 }
 
-void measure_master_scan_yielding_earliest(Database & db, Table & table) {
+int64_t measure_master_scan_yielding_latest(branch_id_t branch, Database & db, Table & table) {
     QueryContext ctx(db);
-    ctx.executionContext.branchId = master_branch_id;
-    db.constructBranchLineage(master_branch_id, ctx.executionContext);
+    ctx.executionContext.branchId = branch;
+    db.constructBranchLineage(branch, ctx.executionContext);
 
     const auto & column0 = table.getColumn(0);
     const auto & column1 = table.getColumn(1);
@@ -186,14 +177,65 @@ void measure_master_scan_yielding_earliest(Database & db, Table & table) {
         {column1, 4},
         {column2, 8});
 
+    tuple_cnt = 0;
+
     using namespace std::chrono;
     const auto query_start = high_resolution_clock::now();
-    scan_relation_yielding_earliest(ctx, table, print_result, scan_items);
+    scan_relation_yielding_latest(ctx, table, sum_consumer, scan_items);
     const auto query_duration = high_resolution_clock::now() - query_start;
-    printf("Execution time: %lums\n", duration_cast<milliseconds>(query_duration).count());
+    auto duration = duration_cast<milliseconds>(query_duration).count();
+//    printf("Execution time: %lums\n", duration);
+    return duration;
+}
+
+int64_t measure_master_scan_yielding_earliest(branch_id_t branch, Database & db, Table & table) {
+    QueryContext ctx(db);
+    ctx.executionContext.branchId = branch;
+    db.constructBranchLineage(branch, ctx.executionContext);
+
+    const auto & column0 = table.getColumn(0);
+    const auto & column1 = table.getColumn(1);
+    const auto & column2 = table.getColumn(2);
+
+    auto scan_items = std::make_tuple<
+        TmplScanItem<Integer>, TmplScanItem<Integer>, TmplScanItem<Integer>>(
+        {column0, 0},
+        {column1, 4},
+        {column2, 8});
+
+    tuple_cnt = 0;
+
+    using namespace std::chrono;
+    const auto query_start = high_resolution_clock::now();
+    scan_relation_yielding_earliest(ctx, table, sum_consumer, scan_items);
+    const auto query_duration = high_resolution_clock::now() - query_start;
+    auto duration = duration_cast<milliseconds>(query_duration).count();
+//    printf("Execution time: %lums\n", duration);
+    return duration;
+}
+
+void measure(std::function<int64_t()> bench_func) {
+    int64_t acc = 0;
+    std::vector<int64_t> samples;
+    for (size_t i = 0; i < repetitions; ++i) {
+        auto sample = bench_func();
+        acc += sample;
+        samples.push_back(sample);
+    }
+    double mean = static_cast<double>(acc)/repetitions;
+    double var = 0.0, std;
+    for (size_t i = 0; i < repetitions; ++i) {
+        var += (samples[i] - mean) * (samples[i] - mean);
+    }
+    std = std::sqrt(var);
+    std::cout << "mean: " << mean << " std: " << std << std::endl;
+    std::cout << "tuple count: " << tuple_cnt << std::endl;
+    double tuple_cost = mean/tuple_cnt*1.e6;
+    std::cout << "per tuple cost: " << tuple_cost << "ns" << std::endl;
 }
 
 void run_benchmark() {
+    printf("generating data...\n");
     auto db = std::make_unique<Database>();
 
     auto & bench_table = db->createTable("bench_table");
@@ -201,22 +243,36 @@ void run_benchmark() {
     bench_table.addColumn("b", Sql::getIntegerTy());
     bench_table.addColumn("c", Sql::getIntegerTy());
 
+    std::vector<branch_id_t> branches{ master_branch_id };
     perform_bunch_inserts(*db, bench_table);
     perform_bunch_updates(*db, bench_table);
     branch_id_t branch1 = db->createBranch("branch1", master_branch_id);
+    branches.push_back(branch1);
     perform_bunch_inserts(*db, bench_table);
     perform_bunch_updates(*db, bench_table);
     branch_id_t branch2 = db->createBranch("branch2", branch1);
+    branches.push_back(branch2);
     perform_bunch_inserts(*db, bench_table);
     perform_bunch_updates(*db, bench_table);
     branch_id_t branch3 = db->createBranch("branch3", invalid_branch_id);
+    branches.push_back(branch3);
     perform_bunch_inserts(*db, bench_table);
     perform_bunch_updates(*db, bench_table);
     branch_id_t branch4 = db->createBranch("branch4", branch3);
+    branches.push_back(branch4);
     perform_bunch_inserts(*db, bench_table);
-    perform_bunch_updates(*db, bench_table);
+    perform_bunch_updates(*db, bench_table);    
 
-    measure_master_scan_yielding_earliest(*db, bench_table);
+    for (branch_id_t branch : branches) {
+        printf("measuring scan performance on branch %d yielding earliest tuples...\n", branch);
+        measure([&]{
+            return measure_master_scan_yielding_earliest(branch, *db, bench_table);
+        });
+        printf("measuring scan performance on branch %d yielding latest tuples...\n", branch);
+        measure([&]{
+            return measure_master_scan_yielding_latest(branch, *db, bench_table);
+        });
+    }
 }
 
 int main(int argc, char * argv[]) {
