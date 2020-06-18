da95590954bbfb364e26d2520d82de7ecc01adce
Thomas Blum <ga96xic@mytum.de>
2020-06-18 16:49:37 +0200

    Modify library linkage

--- test/local.cmake
+++ test/local.cmake
@@ -4,6 +4,6 @@ list(REMOVE_ITEM TEST_CC test/tester.cpp)
 
 ### Executable
 add_executable(tester test/tester.cpp ${TEST_CC})
-target_link_libraries(tester dblib gtest gmock Threads::Threads)
+target_link_libraries(tester dblib queryCompiler queryExecutor gtest gmock Threads::Threads)
 enable_testing()
 add_test(dblib tester)
