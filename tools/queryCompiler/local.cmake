25a3f3ff4830434498e6745c7a3f1e4adeb3c9c6
Thomas Blum <ga96xic@mytum.de>
2020-06-18 16:54:21 +0200

    Link queryCompiler library with dblib

--- tools/queryCompiler/local.cmake
+++ tools/queryCompiler/local.cmake
@@ -13,3 +13,4 @@ file(GLOB_RECURSE SRC_CC "tools/queryCompiler/*.cpp")
 # ---------------------------------------------------------------------------
 
 add_library(queryCompiler STATIC ${SRC_CC})
+target_link_libraries(queryCompiler dblib queryExecutor)
