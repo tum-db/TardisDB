1435976b2e565765a2d12a8f864e71d17a153c1e
Thomas Blum <ga96xic@mytum.de>
2020-09-16 15:41:47 +0200

    Change count of runs

--- benchmark/semanticalBenchmark.sh
+++ benchmark/semanticalBenchmark.sh
@@ -400,21 +400,21 @@ echo "Benchmark Select Statements with branching..."
 benchmark_input_for_distributions b1s_statements $OUTPUT_FILE 2 3
 benchmark_input_for_distributions b2s_statements $OUTPUT_FILE 3 3
 echo "Benchmark Merge Statements..."
-benchmark_input_for_distributions mm_statements $OUTPUT_FILE 4 3
+benchmark_input_for_distributions mm_statements $OUTPUT_FILE 4 1
 echo "Benchmark Merge Statements with branching..."
-benchmark_input_for_distributions b1jm_statements $OUTPUT_FILE 5 3
-benchmark_input_for_distributions b2jm_statements $OUTPUT_FILE 6 3
-benchmark_input_for_distributions b1m_statements $OUTPUT_FILE 7 3
-benchmark_input_for_distributions b2m_statements $OUTPUT_FILE 8 3
+benchmark_input_for_distributions b1jm_statements $OUTPUT_FILE 5 1
+benchmark_input_for_distributions b2jm_statements $OUTPUT_FILE 6 1
+benchmark_input_for_distributions b1m_statements $OUTPUT_FILE 7 1
+benchmark_input_for_distributions b2m_statements $OUTPUT_FILE 8 1
 echo "Benchmark Update Statements..."
 benchmark_input_for_distributions mu_statements $OUTPUT_FILE 9 3
 echo "Benchmark Update Statements with branching..."
 benchmark_input_for_distributions b1u_statements $OUTPUT_FILE 10 3
 benchmark_input_for_distributions b2u_statements $OUTPUT_FILE 11 3
 echo "Benchmark Insert Statements..."
-benchmark_input mi_statements $OUTPUT_FILE 12 1 "0.5" 30227 30303
+benchmark_input "./benchmarkStatements/mi_statements_30227_30303.txt" $OUTPUT_FILE 12 1 "0.5" 30227 30303
 echo "Benchmark Insert Statements with branching..."
-benchmark_input bi_statements $OUTPUT_FILE 13 1 "0.5" 30227 30303
+benchmark_input "./benchmarkStatements/bi_statements_30227_30303.txt" $OUTPUT_FILE 13 1 "0.5" 30227 30303
 echo "Benchmark Delete Statements..."
 benchmark_input_for_distributions md_statements $OUTPUT_FILE 14 1
 echo "Benchmark Delete Statements with branching..."
