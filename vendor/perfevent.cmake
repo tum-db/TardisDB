925c311a3098267690e8636bf00a05c056a8811a
Thomas Blum <ga96xic@mytum.de>
2020-09-18 11:53:16 +0200

    Add include dir

--- vendor/perfevent.cmake
+++ vendor/perfevent.cmake
@@ -7,7 +7,7 @@ ExternalProject_Add(
         perfevent
         PREFIX "vendor/perfevent"
         GIT_REPOSITORY "https://github.com/viktorleis/perfevent.git"
-        GIT_TAG f34b43af69e45521b973b4065e25504c3ec43d93
+        GIT_TAG master
         TIMEOUT 10
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ""
@@ -20,4 +20,4 @@ ExternalProject_Get_Property(perfevent install_dir)
 set(PERFEVENT_INCLUDE_DIR ${install_dir}/include)
 file(MAKE_DIRECTORY ${PERFEVENT_INCLUDE_DIR})
 file(COPY ${install_dir}/src/perfevent DESTINATION ${PERFEVENT_INCLUDE_DIR})
-include_directories(${PERFEVENT_INCLUDE_DIR})
+#include_directories(${PERFEVENT_INCLUDE_DIR})
